a368337d664b06798c4da0d181b944a1dcf2fbb0
Kautilya <kautilyasave@gmail.com>
2020-03-24 13:41:24 -0400

    (UI)iOS: Initial commit

---
+++ src/iOS/HealthSense/HealthSense WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  HealthSense WatchKit Extension
+//
+//  Created by Sensehack on 3/24/20.
+//  Copyright © 2020 Sensehack. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ src/iOS/HealthSense/HealthSense/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HealthSense
+//
+//  Created by Sensehack on 3/24/20.
+//  Copyright © 2020 Sensehack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
