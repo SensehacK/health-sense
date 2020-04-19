73b8ade9f84200c0cab1523198db18e204ed4e93
Kautilya <kautilyasave@gmail.com>
2020-04-19 12:35:25 -0400

    (feat)Workout: Recording

--- src/iOS/HealthSense/HealthSense WatchKit Extension/InterfaceController.swift
+++ src/iOS/HealthSense/HealthSense WatchKit Extension/InterfaceController.swift
@@ -8,7 +8,7 @@
 
 import WatchKit
 import Foundation
-
+import HealthKit
 
 class InterfaceController: WKInterfaceController {
 
@@ -16,12 +16,28 @@ class InterfaceController: WKInterfaceController {
     
     @IBOutlet weak var workoutButton: WKInterfaceButton!
     
+    let healthKitManager = HealthKitManager.sharedInstance
+    
     var isWorkoutInProgress = false
+    
+    var workoutSession: HKWorkoutSession?
+    
+    var workOutStartDate: Date?
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
+        self.workoutButton.setEnabled(false)
+        
         // Configure interface objects here.
+        
+        healthKitManager.authorizeHealthKit { (success, error) in
+            print("Healthkit successful ?", success)
+            self.workoutButton.setEnabled(true)
+            self.createWorkoutSession()
+        }
+        
+        
     }
     
     override func willActivate() {
@@ -39,12 +55,98 @@ class InterfaceController: WKInterfaceController {
         print("Button tapped")
         if isWorkoutInProgress {
             print("End Workout")
+            endWorkoutSession()
         } else {
             print("Start Workout")
+            startWorkoutSession()
         }
         isWorkoutInProgress = !isWorkoutInProgress
         self.workoutButton.setTitle(isWorkoutInProgress ? "End Workout" : "Start Workout")
         
     }
+    
+    func createWorkoutSession() {
+        
+        let workoutConfiguration = HKWorkoutConfiguration()
+        workoutConfiguration.activityType = .other
+        workoutConfiguration.locationType = .unknown
+        
+        
+        do {
+//            workoutSession = try HKWorkoutSession(configuration: workoutConfiguration)
+            workoutSession = try HKWorkoutSession(healthStore: self.healthKitManager.healthStore, configuration: workoutConfiguration)
+            workoutSession?.delegate = self
+        } catch {
+            print("Exception thrown")
+        }
+    }
+    
+    func startWorkoutSession() {
+        if self.workoutSession == nil {
+            createWorkoutSession()
+        }
+        
+        guard let session = workoutSession else {
+            print("cannot start workout session wihout a workout session")
+            return
+        }
+        
+        self.workOutStartDate = Date()
+//        healthKitManager.healthStore.start(session)
+        
+//        session.startActivity(with: Date.init())
+        session.startActivity(with: self.workOutStartDate)
+
+                
+    }
+    
+    func endWorkoutSession() {
+        guard let session = workoutSession else {
+            print("Cannot start workout without a session")
+            return
+        }
+        
+        session.end()
+        saveWorkoutSession()
+        
+    }
+    
+    func saveWorkoutSession() {
+        
+        guard let startDate = workOutStartDate else {
+            return
+        }
+        
+        let workout = HKWorkout(activityType: .other, start: startDate, end: Date())
+        
+        healthKitManager.healthStore.save(workout) { (success, error) in
+            print("Did we save the workout? You Decide!\(success)")
+        }
+    }
+    
+}
+
+extension InterfaceController: HKWorkoutSessionDelegate {
+    
+    
+    func workoutSession(_ workoutSession: HKWorkoutSession, didChangeTo toState: HKWorkoutSessionState, from fromState: HKWorkoutSessionState, date: Date) {
+        
+        print("In workoutsession Interface Controller Delegates")
+        switch toState {
+        case .running:
+            print("workout started")
+        case .ended:
+            print("workout ended")
+        default:
+            print("other workout started")
+        }
+    }
+    
+    func workoutSession(_ workoutSession: HKWorkoutSession, didFailWithError error: Error) {
+        print("Workout failed with error \(error)")
+    }
+    
+    
+    
     
 }
