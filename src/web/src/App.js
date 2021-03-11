35d9ec42e5c2db73df4f1e10494414e9d7587936
Abbas Shamshi <abbasshamshi72@gmail.com>
2021-03-11 14:47:34 -0500

    Initial Commit

---
+++ src/web/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Health Sense</p>
+        </header>
+      </div>
+    );
+
+  }
+}
